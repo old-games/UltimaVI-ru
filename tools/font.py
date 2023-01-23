2f08b3105aa60768e116e652bb94ff12d12d5131
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2023-01-23 16:11:36 +0300

    Unpacked character table.

---
+++ tools/font.py
@@ -0,0 +1,14 @@
+import sys
+
+
+with open(sys.argv[1], 'rb') as f:
+    x = f.read()
+
+for i in range(64):
+    if i % 8 == 0:
+        print(f'{i // 8 * 32:02X}')
+
+    for j in range(256):
+        c = i // 8 * 32 + j // 8
+        print(' x'[bool(x[c*8+(i % 8)] & (1 << (8-j%8)))], end='')
+    print()
