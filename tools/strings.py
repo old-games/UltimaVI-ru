b1c5505de2418114e9f4d92d0fb4889fc4b2e548
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2023-01-29 20:45:17 +0300

    Saved strings.

--- tools/strings.py
+++ tools/strings.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 
@@ -21,6 +22,11 @@ def read_null_terminated(d, i):
     return s
 
 
+with open('tools/translation.json') as f:
+    t = json.loads(f.read())
+
+tt = {(x['source'], x['offset']): (x['english'], x['russian']) for x in t}
+
 for name in ds:
     with open(f'unpacked/{name}', 'rb') as f:
         d = f.read()
@@ -30,14 +36,22 @@ for name in ds:
             if d[i-1] == 0x50 and d[i-4] == 0xb8 and d[i-5] == 0x50 and d[i-8] == 0xb8:
                 a = int.from_bytes(d[i-7:i-5], 'little')
                 a2 = int.from_bytes(d[i-3:i-1], 'little')
-                print(hex(i), repr(read_null_terminated(d, a*0x10+a2+0x3600)))
+                if (name, a*0x10+a2+0x3600) not in tt:
+                    s = read_null_terminated(d, a*0x10+a2+0x3600)
+                    tt[(name, a*0x10+a2+0x3600)] = s, 'FIXME ' + s
 
             elif d[i-1] == 0x50 and d[i-4] == 0xb8 and d[i-5] == 0x1e:
                 a = ds[name]
                 a2 = int.from_bytes(d[i-3:i-1], 'little')
-                print(hex(i), repr(read_null_terminated(d, a*0x10+a2+0x3600)))
+                if (name, a*0x10+a2+0x3600) not in tt:
+                    s = read_null_terminated(d, a*0x10+a2+0x3600)
+                    tt[(name, a*0x10+a2+0x3600)] = s, 'FIXME ' + s
 
             else:
                 print(hex(i), hex(d[i-1]), 'UNKNOWN')
 
 # TODO %s
+
+t = [{'source': s, 'offset': o, 'english': en, 'russian': ru} for (s, o), (en, ru) in sorted(tt.items())]
+with open('tools/translation.json', 'w') as f:
+    f.write(json.dumps(t, indent=4, ensure_ascii=False))
