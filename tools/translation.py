751e074cced5e2d5e38cc0b2b2ad30a6bec10d25
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2023-02-03 19:55:04 +0300

    Better translation check.

--- tools/translation.py
+++ tools/translation.py
@@ -1,13 +1,33 @@
+import collections
 import json
 import os
 
 
 os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+
+def read_null_terminated(d, i):
+    s = ''
+    while d[i]:
+        s += d[i:i+1].decode()
+        i += 1
+    return s
+
+
 with open('tools/translation.json') as f:
     t = json.loads(f.read())
 
+by_source = collections.defaultdict(list)
+
 for i in t:
     assert set(i.keys()) == {'source', 'offset', 'english', 'russian'}
-    assert i['source'] in {'GAME.EXE', 'END.EXE', 'INSTALL.EXE', 'U.EXE', 'ULTIMA6.EXE'}
+    assert i['source'] in {'GAME.EXE', 'END.EXE', 'INSTALL.EXE', 'U.EXE', 'ULTIMA6.EXE'}, f'Invalid source: "{i["source"]}".'
     assert isinstance(i['offset'], int)
+    by_source[i['source']].append(i)
+
+for s, ii in by_source.items():
+    with open(f'unpacked/{s}', 'rb') as f:
+        x = f.read()
+    for i in ii:
+        text = read_null_terminated(x, i['offset'])
+        assert text == i['english'], f'English text could not be found at offset {i["offset"]}: "{i["english"]}".'
