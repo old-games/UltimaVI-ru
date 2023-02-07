594551b2bc1c419193674d4461633b480e01eef1
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2023-02-07 11:29:43 +0300

    More strings.

--- tools/strings.py
+++ tools/strings.py
@@ -6,7 +6,7 @@ import subprocess
 os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
-ds = {
+dsegs = {
     'END.EXE': 0x07b4,
     'GAME.EXE': 0x2d85,
     'U.EXE': 0x1499,
@@ -21,10 +21,17 @@ printf = {
 
 def read_null_terminated(d, i):
     s = ''
+    assert d[i] != 0
     while d[i]:
-        s += d[i:i+1].decode()
+        assert d[i] in b'\r\n\b' + bytes(range(0x20, 0x80))
+        s += d[i:i+1].decode('ascii')
         i += 1
     return s
+
+
+def add_string(name, offset, s):
+    if (name, offset) not in tt:
+        tt[(name, offset)] = (s, f'FIXME {s}')
 
 
 with open('tools/translation.json') as f:
@@ -32,32 +39,55 @@ with open('tools/translation.json') as f:
 
 tt = {(x['source'], x['offset']): (x['english'], x['russian']) for x in t}
 
-for name in ds:
+for name, ds in dsegs.items():
     with open(f'unpacked/{name}', 'rb') as f:
         d = f.read()
 
     base = int.from_bytes(d[8:0x0a], 'little')*0x10
 
     calls = [b'\x9a' + func.to_bytes(4, 'little') for func in printf[name]]
-    for i in range(len(d)):
-        if d[i:i+5] in calls:
-            print(name, hex(i))
+    for i in range(base, ds*0x10+base):
+        if d[i] == 0x9a:
+            is_printf = d[i:i+5] in calls
             if d[i-1] == 0x50 and d[i-4] == 0xb8 and d[i-5] == 0x50 and d[i-8] == 0xb8:
                 a = int.from_bytes(d[i-7:i-5], 'little')
                 a2 = int.from_bytes(d[i-3:i-1], 'little')
-                if (name, a*0x10+a2+base) not in tt:
+                try:
                     s = read_null_terminated(d, a*0x10+a2+base)
-                    tt[(name, a*0x10+a2+base)] = s, 'FIXME ' + s
+                    add_string(name, a*0x10+a2+base, s)
+                except (IndexError, UnicodeDecodeError, AssertionError):
+                    pass
 
             elif d[i-1] == 0x50 and d[i-4] == 0xb8 and d[i-5] == 0x1e:
-                a = ds[name]
                 a2 = int.from_bytes(d[i-3:i-1], 'little')
-                if (name, a*0x10+a2+base) not in tt:
-                    s = read_null_terminated(d, a*0x10+a2+base)
-                    tt[(name, a*0x10+a2+base)] = s, 'FIXME ' + s
+                try:
+                    s = read_null_terminated(d, ds*0x10+a2+base)
+                    add_string(name, ds*0x10+a2+base, s)
+                except (IndexError, UnicodeDecodeError, AssertionError):
+                    print(i)
+                    pass
 
-            else:
+            elif printf:
                 print(name, hex(i), hex(d[i-1]), 'UNKNOWN')
+
+    for i in range(ds*0x10+base, len(d), 2):
+        a2 = int.from_bytes(d[i:i+2], 'little')
+        try:
+            assert d[ds*0x10+a2+base-1] == 0
+            s = read_null_terminated(d, ds*0x10+a2+base)
+            add_string(name, ds*0x10+a2+base, s)
+        except (IndexError, UnicodeDecodeError, AssertionError):
+            pass
+
+    for i in range(base, ds*0x10+base):
+        if d[i] == 0xb8:
+            a2 = int.from_bytes(d[i+1:i+3], 'little')
+            try:
+                assert d[ds*0x10+a2+base-1] == 0
+                s = read_null_terminated(d, ds*0x10+a2+base)
+                add_string(name, ds*0x10+a2+base, s)
+            except (IndexError, UnicodeDecodeError, AssertionError):
+                pass
 
 # TODO %s
 
@@ -66,10 +96,10 @@ with open('tools/translation.json', 'w') as f:
     f.write(json.dumps(t, indent=4, ensure_ascii=False))
 
 c = 0
-for name in ds:
+for name in dsegs:
     r = subprocess.run(['strings', f'unpacked/{name}'], stdout=subprocess.PIPE, universal_newlines=True)
     r.check_returncode()
-    t = {e for (s, _), (e, _) in tt.items() if s == name}
+    t = {e.replace('\n', '').replace('\r', '').replace('\b', '') for (s, _), (e, _) in tt.items() if s == name}
     for i in r.stdout.splitlines():
         if i not in t:
             print(name, 'Not found:', i)
