7c52b5665ed4040398f75bad13a62ad45ad45f6c
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2023-02-19 04:27:28 +0300

    Cyrillic letters from another font.

--- tools/replace-cyrillic.py
+++ tools/replace-cyrillic.py
@@ -10,6 +10,9 @@ with open('original/U6.CH', 'rb') as f:
 with open('tools/cp866-8x8', 'rb') as f:
     e = f.read()
 
+with open('tools/atari-st-8x8', 'rb') as f:
+    g = f.read()
+
 os.chdir(output_directory)
 with open('U6.CH', 'wb') as f:
-    f.write(d[:1024] + e[1024:] + d[1024:])
+    f.write(d[:1024] + g[1536:1920] + e[1408:1792] + g[1920:] + g[1576:1584] + bytes((x >> 1 for x in g[1096:1104])) + e[1936:] + d[1024:])
