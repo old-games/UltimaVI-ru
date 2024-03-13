e38721833ee960c479385aca7b4bc659e28aac84
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2024-03-14 02:07:32 +0300

    Better font example.

--- tools/font.py
+++ tools/font.py
@@ -7,14 +7,30 @@ if ext == '.CH':
     with open(sys.argv[1], 'rb') as f:
         x = f.read()
 
-    for i in range((len(x) + 31) // 32):
-        if i % 8 == 0:
-            print(f'{i // 8 * 32:02X}')
+    width = 9
+    result = []
+    for r in range(len(x) // 128):
+        result.append([f'{r*0x10:02X}'])
+        for k in range(4):
+            result.append([' ']*width*16)
 
-        for j in range(256):
-            c = i // 8 * 32 + j // 8
-            print(' █'[bool(x[c*8+(i % 8)] & (1 << (7-j%8)))], end='')
-        print()
+    for i in range(len(x) // 128):
+        for k in range(8):
+            for j in range(16):
+                for l in range(8):
+                    c = i * 16 + j
+                    C = x[c*8+k] & (1 << (7-l%8))
+                    if C:
+                        r = result[1+k//2+i*5][j*width+l]
+                        if k % 2 == 0:
+                            result[1+k//2+i*5][j*width+l] = '▀'
+                        elif r == ' ':
+                            result[1+k//2+i*5][j*width+l] = '▄'
+                        else:
+                            result[1+k//2+i*5][j*width+l] = '█'
+
+    for l in result:
+        print(''.join(l))
 
 elif ext == '.SET':
     with open(sys.argv[1], 'rb') as f:
@@ -22,10 +38,10 @@ elif ext == '.SET':
 
     width = max(s[4:0x104]) + 1
     result = []
-    for r in range(8):
-        result.append([f'{r*0x20:02X}'])
-        for k in range(8):
-            result.append([' ']*width*32)
+    for r in range(16):
+        result.append([f'{r*0x10:02X}'])
+        for k in range(4):
+            result.append([' ']*width*16)
 
     for i, (lo, hi, size) in enumerate(zip(s[0x104:0x204], s[0x204:0x304], s[4:0x104])):
         offset = hi*0x100+lo
@@ -34,10 +50,17 @@ elif ext == '.SET':
 
         for j in range(8):
             for k in range(size):
+
                 c = s[offset+j*size+k]
                 assert c in (0, 15)
                 if c:
-                    result[1+j+i//32*9][i%32*width+k] = '█'
+                    r = result[1+j//2+i//16*5][i%16*width+k]
+                    if j % 2 == 0:
+                        result[1+j//2+i//16*5][i%16*width+k] = '▀'
+                    elif r == ' ':
+                        result[1+j//2+i//16*5][i%16*width+k] = '▄'
+                    else:
+                        result[1+j//2+i//16*5][i%16*width+k] = '█'
 
     for l in result:
         print(''.join(l))
