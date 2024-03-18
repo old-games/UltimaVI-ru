63b33bd8af5696b8cdd58346922da9a13fc7f624
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2024-03-18 14:04:25 +0300

    Strict unpacking of conversations and portraits.

--- tools/archive.py
+++ tools/archive.py
@@ -1,3 +1,4 @@
+import hashlib
 import os
 
 import tools
@@ -7,59 +8,49 @@ import tools.lzw
 def list(path):
     items = []
     offsets = {}
-    current = 0
     with open(path, 'rb') as f:
-        while True:
-            offset = int.from_bytes(f.read(4), 'little')
-            current += 4
-            if current in offsets:
-                break
-            elif offset != 0:
-                assert offset not in offsets
-                offsets[offset] = len(items)
-            items.append(None)
-        size = os.path.getsize(path)
-        assert size not in offsets
-        offsets[size] = None
+        data = bytearray(f.read())
 
-        offsets_right = iter(offsets)
-        next(offsets_right)
-        for (left, index), right in zip(offsets.items(), offsets_right):
-            assert right >= left
-            f.seek(left)
-            items[index] = f.read(right-left)
+    current = 0
+    while current < len(data):
+        offset = int.from_bytes(data[current:current+4], 'little')
+        current += 4
+        if current in offsets:
+            break
+        elif offset != 0:
+            assert offset not in offsets
+            offsets[offset] = len(items)
+        items.append(None)
 
-    basename = os.path.basename(path)
-    fixed_items = []
-    for i, data in enumerate(items):
-        if data:
-            if basename == 'CONVERSE.A' and i == 97:
-                assert len(items) == i+1 or items[i+1] is None
-                data, rest = data[:-1397], data[-1397:]
-                fixed_items.append(data)
-                fixed_items.append(rest)
-            else:
-                fixed_items.append(data)
-        else:
-            fixed_items.append(None)
+    if hashlib.sha256(data).hexdigest() == '8e4620f39d62734fbe4de29e5fa28103da706c6dcf08cfcf6d12c71070faf274':
+        # Missed conversation offset in `CONVERSE.A`.
+        offsets[0x275c2] = len(items)
+        items.append(None)
 
-    return fixed_items
+    size = len(data)
+    assert size not in offsets
+    offsets[size] = None
+
+    offsets_right = iter(offsets)
+    next(offsets_right)
+    for (left, index), right in zip(offsets.items(), offsets_right):
+        assert right >= left
+        items[index] = data[left:right]
+
+    return items
 
 
 def unpack(path):
-    basename = os.path.basename(path)
-    name = os.path.splitext(basename)[0]
+    name = os.path.splitext(os.path.basename(path))[0]
 
     result = []
-    for i, data in enumerate(list(path)):
-        if data:
-            if basename == 'CONVERSE.B' and i == 26 and int.from_bytes(data[:4], 'little') == 0:
-                result.append(data[4:])
-            elif name == 'PORTRAIT':
-                result.append(data)
-            else:
-                result.append(tools.lzw.decompress(data))
+    for data in list(path):
+        if data is None or name == 'PORTRAIT':
+            result.append(data)
+        elif hashlib.sha256(data).hexdigest() == '52484ae256cef6191b79c2a09c070198cf1fdc067675ebc54fe930093f0409a7':
+            # Uncompressed entry in `CONVERSE.B`.
+            result.append(data[4:])
         else:
-            result.append(None)
+            result.append(tools.lzw.decompress(data))
 
     return result
