9795f8150627c018ed9dfa9b77804bc52666bf8d
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2024-03-27 22:01:26 +0300

    Added portraits.

---
+++ tests/test_archive.py
@@ -0,0 +1,23 @@
+import os
+import tempfile
+
+import tests
+import tools.archive
+
+
+class TestArchive(tests.TestCase):
+    def testDecodeEncode(self):
+        with tempfile.TemporaryDirectory() as d:
+            for name in ('CONVERSE.A', 'CONVERSE.B', 'PORTRAIT.A', 'PORTRAIT.B', 'PORTRAIT.Z'):
+                with self.subTest(name=name):
+                    path = os.path.join(self.script_path, 'original', name)
+                    with open(path, 'rb') as f:
+                        expected = f.read()
+
+                    ours_path = os.path.join(d, name)
+                    data = tools.archive.decode(path)
+                    tools.archive.encode(data, ours_path)
+                    with open(ours_path, 'rb') as f:
+                        ours = f.read()
+
+                    self.assertEqual(ours, expected)
