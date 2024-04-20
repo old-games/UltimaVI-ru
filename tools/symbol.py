db8bda9247ed2dcec576f5cff2203658a682f510
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2024-04-20 14:36:57 +0300

    Converted fonts to text and reset images.

--- tools/symbol.py
+++ tools/symbol.py
@@ -1,9 +1,12 @@
+import collections
+
 import PIL.Image
 import PIL.ImageDraw
 import PIL.ImageFont
 
 
 def make_image(input_path, output_path, symbol_width):
+    # FIXME auto-guess symbol_width
     size = 15
     factor = size+1
     with open(input_path) as f:
@@ -47,3 +50,78 @@ def make_image(input_path, output_path, symbol_width):
                 draw.text((column*factor+1, (row//5*8+row//5+1)*factor-2), f'{code+column//symbol_width:02X}', fill='red', anchor='ls', font=font)
 
     image.save(output_path)
+
+
+def read_image(input_path, output_path):
+    columns = 0x10
+    size = 15
+    factor = size+1
+
+    def get_colors(image):
+        colors = collections.Counter()
+        pixels = image.load()
+        for x in range(image.width):
+            for y in range(image.height):
+                colors[pixels[(x, y)]] += 1
+        return colors
+
+    image = PIL.Image.open(input_path)
+    assert (image.width + 1) % factor == 0
+    assert (image.height + 1) % factor == 0
+    width = (image.width + 1) // factor
+    height = (image.height + 1) // factor
+    assert width % columns == 0
+    assert height % 9 == 0
+    symbol_width = width // columns
+    rows = height // 9
+    row_height = 9 * factor
+
+    colors = [[None]*symbol_width*columns for row in range(rows*8)]
+    for row in range(rows):
+        row_image = image.crop((0, row_height*row+factor, image.width, row_height*(row+1)-1))
+        for column in range(columns):
+            symbol = row_image.crop((symbol_width*factor*column, 0, symbol_width*factor*(column+1) - 1, row_image.height))
+            for x in range(symbol_width):
+                for y in range(8):
+                    pixel = symbol.crop((x*factor, y*factor, (x+1)*factor-1, (y+1)*factor-1))
+                    (color, _), = get_colors(pixel).most_common(1)
+                    color = color[:3]
+                    color_names = {
+                        (0, 0, 0): 'black',
+                        (255, 255, 255): 'white',
+                        (255, 224, 224): 'pink'
+                    }
+                    assert color in color_names
+                    colors[row*8+y][column*symbol_width+x] = color_names[color]
+
+    with open(output_path, 'w') as f:
+        for row in range(rows):
+            rows_data = [['.']*symbol_width*columns for _ in range(4)]
+            for column in range(columns):
+                widths = set()
+                for y in range(8):
+                    for x in range(symbol_width):
+                        if colors[row*8+y][column*symbol_width+x] == 'pink':
+                            widths.add(x)
+                            break
+                    else:
+                        widths.add(symbol_width)
+                assert len(widths) == 1
+                width, = widths
+
+                for y in range(8):
+                    for x in range(width):
+                        if colors[row*8+y][column*symbol_width+x] == 'black':
+                            if y % 2 == 0:
+                                rows_data[y//2][column*symbol_width+x] = '▀'
+                            elif rows_data[y//2][column*symbol_width+x] in ' .':
+                                rows_data[y//2][column*symbol_width+x] = '▄'
+                            else:
+                                rows_data[y//2][column*symbol_width+x] = '█'
+                        else:
+                            if y % 2 == 0:
+                                rows_data[y//2][column*symbol_width+x] = ' '
+
+            print(f'{row*0x10:02X}', file=f)
+            for row_data in rows_data:
+                print(''.join(row_data), file=f)
