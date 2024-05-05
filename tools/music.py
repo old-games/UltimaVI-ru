1113086b7d40325cd02227b46e064517c662783e
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2024-05-05 21:04:00 +0300

    sdfsdf

--- tools/music.py
+++ tools/music.py
@@ -7,11 +7,11 @@ def decode(data):
         'modulator',
         'portamento',
         'vibrato',
-        'instrument',
     ]
-    result = []
+    result = [{'command': 'metadata'}]
     index = 0
     offsets = {}
+    # FIXME data from MIDI.DAT
     while index < len(data):
         offsets[index] = len(result)
         command = data[index]
@@ -26,23 +26,42 @@ def decode(data):
             tone = data[index]
             tone_low = tone & 0x1f
             assert tone_low < 24
+            key = tone_low & 7
+            shift = tone_low >> 3
+            assert shift in (0, 1, 2)
+            if key == 0:
+                key = None
+            else:
+                key = ' CDEFGAB'[key] + '♭♯'[2-shift:3-shift]
             octave = tone >> 5
             index += 1
-            result.append({'command': commands[high], 'channel': low, 'octave': octave, 'tone': tone_low})
+            result.append({'command': commands[high], 'channel': low, 'octave': octave, 'tone': key})
 
-        elif high < 8:
+        elif high == 3:
+            arg = data[index]
+            assert arg <= 100
+            index += 1
+            result.append({'command': 'carrier', 'channel': low, 'volume': 100-arg})
+
+        elif high < 7:
             arg = data[index]
             index += 1
             result.append({'command': commands[high], 'channel': low, 'argument': arg})
+
+        elif high == 7:
+            arg = data[index]
+            index += 1
+            result.append({'command': 'instrument', 'channel': low, 'instrument': arg})
 
         elif high == 8:
             assert low in (1, 2, 3, 5, 6), hex(low)
             if low == 1:
                 count = data[index]
+                assert count > 0
                 index += 1
                 start = int.from_bytes(data[index:index+2], 'little')
                 index += 2
-                result.append({'command': 'repeat', 'count': count, 'start': start})
+                result.append({'command': 'call', 'count': count, 'index': start})
 
             elif low == 2:
                 arg = data[index]
@@ -58,17 +77,23 @@ def decode(data):
 
             elif low == 5:
                 arg = data[index]
+                channel = arg >> 4
+                duration = arg & 0x0f
+                assert channel < 9
                 index += 1
-                result.append({'command': 'fade_in', 'argument': arg})
+                result.append({'command': 'fade_in', 'channel': channel, 'duration': duration})
 
             else:
                 assert low == 6
                 arg = data[index]
+                channel = arg >> 4
+                duration = arg & 0x0f
+                assert channel < 9
                 index += 1
-                result.append({'command': 'fade_out', 'argument': arg})
+                result.append({'command': 'fade_out', 'channel': channel, 'duration': duration})
 
         elif high == 0x0e:
-            result.append({'command': 'loop'})
+            result.append({'command': 'continue'})
 
         elif high == 0x0f:
             result.append({'command': 'return'})
@@ -76,7 +101,7 @@ def decode(data):
     assert index == len(data)
 
     for item in result:
-        if item['command'] == 'repeat':
-            item['start'] = offsets[item['start']] + 1
+        if item['command'] == 'call':
+            item['index'] = offsets[item['index']]
 
     return result
