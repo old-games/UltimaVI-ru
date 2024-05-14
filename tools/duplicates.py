812128645f5b1dee2090a721035bd3e4d4b5f1a4
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2024-05-14 10:17:23 +0300

    Draft.

--- tools/duplicates.py
+++ tools/duplicates.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sys
 
 
 output_directory = os.getcwd()
@@ -9,14 +10,25 @@ with open('patches/references.json') as f:
     references = {(x['source'], x['offset']): x['references'] for x in json.loads(f.read())}
 
 with open('patches/bad-references.json') as f:
-    for x in json.loads(f.read()):
-        bad = set(x['bad-references'])
-        references[(x['source'], x['offset'])] = [
-            y
-            for y in references[(x['source'], x['offset'])]
-            if y['origin'] not in bad
-        ]
+    br = json.loads(f.read())
+
+for x in br:
+    bad = set(x['bad-references'])
+    references[(x['source'], x['offset'])] = [
+        y
+        for y in references[(x['source'], x['offset'])]
+        if y['origin'] not in bad
+    ]
 
 for k, v in references.items():
     if len(v) > 1:
         print('WARNING', k[0], hex(k[1]), len(v), 'references:', ', '.join((hex(x['origin']) for x in v)))
+
+if sys.argv[1:] == ['--fix']:
+    for k, v in references.items():
+        if len(v) > 1:
+            br.append({'source': k[0], 'offset': k[1], 'bad-references': [x['origin'] for x in v]})
+
+    br = sorted(br, key=lambda x: (x['source'], x['offset'])) 
+    with open('patches/bad-references.json', 'w') as f:
+        f.write(json.dumps(br, indent=4))
